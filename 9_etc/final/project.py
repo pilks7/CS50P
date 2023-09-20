333b11cb4a6d4fbf135f3c55f16087614db308f3
pilks7 <7pilks7@gmail.com>
2023-09-20 16:32:53 +0100

    Added functionality to project.py - 'create a yearly crop plan

--- 9_etc/final/project.py
+++ 9_etc/final/project.py
@@ -1,3 +1,4 @@
+import random
 from prettytable import from_csv
 from prettytable import PrettyTable
 import os
@@ -21,39 +22,39 @@ class Vegetable:
 
 
 def clean_month_input(input_month):
-    month_mapping = {
-        "january": "Jan",
-        "february": "Feb",
-        "march": "Mar",
-        "april": "Apr",
-        "may": "May",
-        "june": "Jun",
-        "july": "Jul",
-        "august": "Aug",
-        "september": "Sep",
-        "october": "Oct",
-        "november": "Nov",
-        "december": "Dec"
-    }
-    
-    cleaned_month = input_month.lower()
-    
-    # Check if input_month is already a valid abbreviation
-    if cleaned_month in month_mapping.values():
-        return cleaned_month  # Return it as is
-    
-    # Check if input_month is a numeric value
-    if cleaned_month.isdigit():
-        month_index = int(cleaned_month) - 1
-        if 0 <= month_index < 12:
-            return month_mapping[list(month_mapping.keys())[month_index]]  # Return it as is
-    
-    # Check if input_month is a full month name
-    cleaned_month = month_mapping.get(cleaned_month)
-    if cleaned_month:
-        return cleaned_month  # Return it as is
-    else:
-        return input_month.capitalize()  # Capitalize the first letter and return it
+	month_mapping = {
+		"january": "Jan",
+		"february": "Feb",
+		"march": "Mar",
+		"april": "Apr",
+		"may": "May",
+		"june": "Jun",
+		"july": "Jul",
+		"august": "Aug",
+		"september": "Sep",
+		"october": "Oct",
+		"november": "Nov",
+		"december": "Dec"
+	}
+	
+	cleaned_month = input_month.lower()
+	
+	# Check if input_month is already a valid abbreviation
+	if cleaned_month in month_mapping.values():
+		return cleaned_month  # Return it as is
+	
+	# Check if input_month is a numeric value
+	if cleaned_month.isdigit():
+		month_index = int(cleaned_month) - 1
+		if 0 <= month_index < 12:
+			return month_mapping[list(month_mapping.keys())[month_index]]  # Return it as is
+	
+	# Check if input_month is a full month name
+	cleaned_month = month_mapping.get(cleaned_month)
+	if cleaned_month:
+		return cleaned_month  # Return it as is
+	else:
+		return input_month.capitalize()  # Capitalize the first letter and return it
 
 
 
@@ -74,56 +75,121 @@ def get_veg():
 		writer.writerow([crop, sow, plantout, harvests, notes])
 	return Vegetable(crop, sow, plantout, harvests, notes)
 
-def list_crops():
-	if not os.path.isfile('veg.csv') or os.path.getsize("veg.csv") == 0:
+def list_crops(csvf):
+	if not os.path.isfile(csvf) or os.path.getsize(csvf) == 0:
 		print("You have no crops added, add crops at the main menu by pressing '1' ")
 	else:
-		with open("veg.csv", newline='') as csvfile:
+		with open(csvf, newline='') as csvfile:
 			reader = csv.DictReader(csvfile)
 			table = PrettyTable(reader.fieldnames)
 			for row in reader:
 				table.add_row([row[field] for field in reader.fieldnames])
 		print(table)
 
+def year_plan():
+    with open("veg.csv", "r", newline='') as csvfile, open('veg3.csv', 'w', newline='') as out:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        month_dict = {}
+        
+        for row in reader:
+            month = row[3]  # Assuming 'Harvest' is in the fourth column
+            if month not in month_dict:
+                month_dict[month] = [row]
+            else:
+                month_dict[month].append(row)
+        
+        field_names = ["Crop", "Sow", "Plant out", "Harvest", "Notes"]
+        table = PrettyTable(field_names)
+        
+        for month in month_dict:
+            rows = month_dict[month]
+            if len(rows) > 1:
+                random.shuffle(rows)
+                month_dict[month] = [random.choice(rows)]
+        
+        for month in month_dict:
+            row = month_dict[month][0]
+            table.add_row([row[field_names.index(field)] for field in field_names])
+        
+        print(table)
+			
+
+def search():
+	search = input("Type a crop to search: ").strip().capitalize()
+	with open('veg.csv', 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		next(reader)
+		for row in reader:
+			if search == row[0]:
+				table = PrettyTable()  # Create a new PrettyTable
+				table.field_names = ['Crop', 'Sow', 'Plant out', 'Harvest', 'Notes']
+				table.add_row(row)  # Add the matching row to the table
+				print(table)
+			else:
+				if input('Not found. Would you like to add this crop now? Y/N').lower()[:1] in "y":
+					get_veg()
+				else:
+					break
+
+def delete():
+	list_crops()
+	choice = input('Please type which crop to delete. (To delete all, type "A")...').capitalize()
+
+	if choice == 'A':
+		if input('WARNING: This will delete all of your saved crops. Do you wish to continue? Y/N\n').lower()[:1] in "y":
+			os.remove('veg.csv')
+			print('All crops have been deleted')
+	else:
+		with open('veg.csv', 'r', newline='') as csvin, open('veg2.csv', 'w', newline='') as out:
+			reader = csv.reader(csvin)
+			writer = csv.writer(out)
+			header = next(reader)
+			writer.writerow(header)
+
+			found = False
+			for row in reader:
+				if row[0].capitalize() != choice:
+					writer.writerow(row)
+				else:
+					found = True
+
+		os.remove('veg.csv')
+		os.rename('veg2.csv', 'veg.csv')
+
+		if found:
+			print(f"{choice} has been deleted.")
+		else:
+			print(f"{choice} not found in the list.")
+
+
+
+
 def main():
-	choice = ''
-	while choice != 'q':
+	while True:
 		print(f"[1] Add a crop to your list")
 		print(f"[2] List all crops currently in the program")
 		print(f"[3] Create a yearly crop plan")
 		print(f"[4] View a vegetable (or search for it)")
+		print(f"[5] Delete crop(s)")
 		print(f"[q] Enter q to quit.")
-		choice = input("Please choose an option...")
+		choice = input("Please choose an option...").strip().lower()
 
 		match choice:
 			case '1':
 				get_veg()
-				break
 			case '2':
-				list_crops()
-				break
+				list_crops('veg.csv')
 			case '3':
-				
-				break
+				year_plan()
 			case '4':
-				search = input("Type a crop to search: ").strip().capitalize()
-				with open('veg.csv', 'r') as csvfile:
-					reader = csv.reader(csvfile, delimiter=',')
-					next(reader)
-					for row in reader:
-						if search == row[0]:
-							table = PrettyTable()  # Create a new PrettyTable
-							table.field_names = ['Crop', 'Sow', 'Plant out', 'Harvest', 'Notes']
-							table.add_row(row)  # Add the matching row to the table
-							print(table)
-						else:
-							if input('Not found. Would you like to add this crop now? Y/N').lower()[:1] in "y":
-								get_veg()
-							else:
-								break
-				break
+				search()
+			case '5':
+				delete()
 			case 'q':
 				sys.exit(0)
+			case _:
+				print('Invalid option. Please select an option')
 
 if __name__ == '__main__':
 	main()
